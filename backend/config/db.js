bbe6d50fbb7dbe4170e9681051291b815c448bda
Rishu Singh <rishusingh2902@gmail.com>
2026-03-19 19:55:14 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,13 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "1234",
-  host: "localhost",
-  port: 5432,
-  database: "stockmind",
-
-  // Pool settings
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-//////////////////////////////////////////////////
 // TEST CONNECTION
-//////////////////////////////////////////////////
 pool.query("SELECT 1")
   .then(() => {
     console.log("✅ PostgreSQL connected successfully");

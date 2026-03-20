5ee903e9e11208c45fdf76cf5f812a3eea09f6ca
Rishu Singh <rishusingh2902@gmail.com>
2026-03-20 19:11:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ const app = express();
 
 /* ================= CORS ================= */
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-vercel-app.vercel.app"
-  ],
+  origin: "*",
   credentials: true
 }));
 
